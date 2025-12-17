f945e81c67f73848deecee87bea65aeb37ffe515
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2025-12-17 13:46:06 +0100

    Create 1_Control_Operativo.py

---
+++ pages/1_Control_Operativo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="OYKEN · Control Operativo", layout="centered")
+
+st.title("OYKEN · Control Operativo")
+st.info("Página Control Operativo creada correctamente")
