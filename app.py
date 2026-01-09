5e04a46e94d6e2f4f5db69125330c01262a3bc8f
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2026-01-09 12:54:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ OYKEN genera lectura operativa, escenarios de decisión y alerta inteligente ada
 a partir de su realidad diaría.
 
 No sustituye sistemas ya existentes: Los interpreta y los convierte en criterio accionable.
+PROXIMOS MODULOS: Margen ruto-Ebitda Real-Escenarios ... 
 """)
            
 st.markdown("Selecciona un módulo en el menú lateral")
